13ceeb552db1e38632b736c820cd7551164f3c29
Abhishek Rawat <47311875+AbePlays@users.noreply.github.com>
2019-10-20 16:47:22 +0530

    Developing a BitCoin Calculator

---
+++ ByteCoin/ByteCoin/Model/CoinManager.swift
@@ -0,0 +1,66 @@
+import Foundation
+
+protocol CoinManagerDelegate {
+    
+    func didUpdatePrice(price: String, currency: String)
+    func didFailWithError(error: Error)
+}
+
+struct CoinManager {
+    
+    var delegate: CoinManagerDelegate?
+    
+    let baseURL = "https://apiv2.bitcoinaverage.com/indices/global/ticker/BTC"
+    let currencyArray = ["AUD", "BRL","CAD","CNY","EUR","GBP","HKD","IDR","ILS","INR","JPY","MXN","NOK","NZD","PLN","RON","RUB","SEK","SGD","USD","ZAR"]
+    
+    func getCoinPrice(for currency: String) {
+
+        let urlString = baseURL + currency
+        
+        if let url = URL(string: urlString) {
+            
+            let session = URLSession(configuration: .default)
+            
+            let task = session.dataTask(with: url) { (data, response, error) in
+                
+                if error != nil {
+                    self.delegate?.didFailWithError(error: error!)
+                    return
+                }
+                
+                if let safeData = data {
+                    
+                    if let bitcoinPrice = self.parseJSON(safeData) {
+                        
+                        //Optional: round the price down to 2 decimal places.
+                        let priceString = String(format: "%.2f", bitcoinPrice)
+                        
+                        //Call the delegate method in the delegate (ViewController) and
+                        //pass along the necessary data.
+                        self.delegate?.didUpdatePrice(price: priceString, currency: currency)
+                    }
+                }
+                
+            }
+            task.resume()
+        }
+    }
+    
+    func parseJSON(_ data: Data) -> Double? {
+        
+        let decoder = JSONDecoder()
+        do {
+            
+            let decodedData = try decoder.decode(CoinData.self, from: data)
+            
+            let lastPrice = decodedData.last
+            print(lastPrice)
+            return lastPrice
+            
+        } catch {
+            
+            delegate?.didFailWithError(error: error)
+            return nil
+        }
+    }
+}
